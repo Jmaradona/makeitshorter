f2d0a09399988eb7b53be835908032756fa930d5
Jmaradona <88232683+Jmaradona@users.noreply.github.com>
2025-05-18 12:39:47 +0200

    Fix Vite build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +14,7 @@ export default defineConfig({
     sourcemap: true,
     copyPublicDir: true,
     rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'framer-motion'],
